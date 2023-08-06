fe67db36166514b8f2bcf7a03549bb2ab9d85cff
camdotcom14 <camerontarget@gmail.com>
2023-08-06 09:11:31 -0400

    revised

--- hooks/tk-multi-reviewsubmission/submitter_sgtk.py
+++ hooks/tk-multi-reviewsubmission/submitter_sgtk.py
@@ -86,8 +86,6 @@ class SubmitterSGTK(HookBaseClass):
         # create a name for the version based on the file name
         # grab the file name, strip off extension
         name = os.path.splitext(os.path.basename(path_to_movie))[0]
-        # do some replacements
-        name = name.replace()
         # and capitalize
         name = name.capitalize("")
 
