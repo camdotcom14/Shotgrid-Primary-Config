4f59094c77bac7af6bad464ac4cf1fa33a12f21b
camdotcom14 <127965975+camdotcom14@users.noreply.github.com>
2023-08-09 07:06:37 -0700

    changed name of review submit

--- hooks/tk-multi-reviewsubmission/hooks/submitter_sgtk.py
+++ hooks/tk-multi-reviewsubmission/hooks/submitter_sgtk.py
@@ -85,7 +85,7 @@ class SubmitterSGTK(HookBaseClass):
 
         # create a name for the version based on the file name
         # grab the file name, strip off extension
-        name = os.path.splitext(os.path.basename(path_to_movie))[0]
+        name = "code"_"sg_client_version_number"
 
         # Create the version in Shotgun
         ctx = self.__app.context
